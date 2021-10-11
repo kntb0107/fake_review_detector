adb6d8799ddbe09e5b093f5571ad52ec637ef8f5
kntb0107 <72517618+kntb0107@users.noreply.github.com>
2021-10-11 13:39:14 +0400

    Update 4.Deployment.py

--- 4.Deployment.py
+++ 4.Deployment.py
@@ -57,7 +57,7 @@ def main():
     st.title("Fraud Detection in Online Consumer Reviews Using Machine Learning Techniques")
     
     
-    # --CHECK BOXES--
+    # --EXPANDERS--    
     abstract = st.expander("Abstract")
     if abstract:
         abstract.write("In today's world, both businesses and customers believe reviews to be quite beneficial. It's no surprise that review fraud has devalued the whole experience, from nasty reviews putting harm to the business's credibility to breaking international laws. This has been seen as a developing problem, and because it is related to natural language processing, it was critical to develop various machine learning methodologies and techniques to achieve a breakthrough in this sector. Many e-commerce sites, such as Amazon, have their systems in place, including Verified Purchase, which labels review language as accurate when items are purchased directly from the website. This work proposes to use Amazon's verified purchases label to train three classifiers for supervised training on Amazon’s labelled dataset. MNB, SVM, and LR were chosen as classifiers, and model tuning was done using two distinct vectorizers, Count Vectorizer and TF-IDF Vectorizers. Overall, all of the trained models had an accuracy rate of 80%, indicating that the vectorizers functioned admirably and that there are distinctions between false and actual reviews. Out of the two, the count vectorizer improved the models' performance more, and out of the three inside counts, LR performed the best, with an accuracy rate of 85% and a recall rate of 92%. The LR classifier was used, and it was accessible to the public to see if the reviews entered were genuine or not, with a probability score.")
@@ -67,7 +67,8 @@ def main():
     if links:
         links.write("[Dataset utilized](https://www.kaggle.com/akudnaver/amazon-reviews-dataset)")
         links.write("[Github](https://github.com/kntb0107/Fraud-Detection-in-Online-Consumer-Reviews-Using-Machine-Learning-Techniques)")
-
+        
+    # --CHECKBOXES--
     st.subheader("Information on the Classifier")
     if st.checkbox("About Classifer"):
         st.markdown('**Model:** Logistic Regression')
@@ -83,7 +84,6 @@ def main():
         st.markdown('**F-1 Score:** 85%')
 
 
-    
     #--IMPLEMENTATION OF THE CLASSIFIER--
     st.subheader("Fake Review Classifier")
     review = st.text_area("Enter Review: ")
